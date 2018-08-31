14ad273d2b9a0f7b5270efa531941f93912b2497
kcb0126 <kcb0126@outlook.com>
2018-08-31 19:35:21 +0800

    input() method was completed with success test.

--- CleaningRobot.php
+++ CleaningRobot.php
@@ -8,6 +8,11 @@
 
 class CleaningRobot
 {
+    //////////////////////////////// public methods ////////////////////////////////
+
+    /**
+     * CleaningRobot constructor.
+     */
     public function __construct()
     {
     }
@@ -19,7 +24,84 @@ class CleaningRobot
      */
     public function input(array $data)
     {
-        // TODO: implement method to input data from array
-        return false;
+        // input map data from array
+        if (array_key_exists('map', $data)) {
+            $this->map = $data['map'];
+        } else {
+            return false;
+        }
+
+        // input beginning position and direction
+        if (array_key_exists('start', $data)) {
+            $start = $data['start'];
+
+            if (array_key_exists('X', $start)) {
+                $this->currentX = $start['X'];
+            } else {
+                return false;
+            }
+
+            if (array_key_exists('Y', $start)) {
+                $this->currentY = $start['Y'];
+            } else {
+                return false;
+            }
+
+            if (array_key_exists('facing', $start)) {
+                $this->currentFacing = $start['facing'];
+            } else {
+                return false;
+            }
+        } else {
+            return false;
+        } // end of beginning position and direction
+
+        // input commands given to this robot
+        if (array_key_exists('commands', $data)) {
+            $this->remainingCommands = $data['commands'];
+        } else {
+            return false;
+        }
+
+        // input initial battery
+        if (array_key_exists('battery', $data)) {
+            $this->battery = $data['battery'];
+        } else {
+            return false;
+        }
+
+        return true;
     }
+
+    //////////////////////////////////// properties ////////////////////////////////
+
+    /**
+     * @var array
+     */
+    private $map;
+
+    /**
+     * @var int
+     */
+    private $currentX;
+
+    /**
+     * @var int
+     */
+    private $currentY;
+
+    /**
+     * @var string
+     */
+    private $currentFacing;
+
+    /**
+     * @var int
+     */
+    private $battery;
+
+    /**
+     * @var array
+     */
+    private $remainingCommands;
 }
