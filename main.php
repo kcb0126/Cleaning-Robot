88e6d4bfd69508df6b1dd5cefbcfb497c329486d
kcb0126 <kcb0126@outlook.com>
2018-09-01 20:20:24 +0800

    main module is written.

--- main.php
+++ main.php
@@ -5,3 +5,24 @@
  * Date: 8/31/2018
  * Time: 5:41 PM
  */
+
+include_once('CleaningRobot.php');
+
+if($argc < 3) {
+    die("\nphp main.php <source.json> <result.json>\n");
+}
+
+$sourceFile = $argv[1];
+$resultFile = $argv[2];
+
+$inputJSON = file_get_contents($sourceFile);
+
+$robot = new CleaningRobot();
+
+if(!$robot->input(json_decode($inputJSON, true))) {
+    die("\nSource json file is not correct.\n");
+}
+
+$result = $robot->execute();
+
+file_put_contents($resultFile, json_encode($result, JSON_PRETTY_PRINT));
